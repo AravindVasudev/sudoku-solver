beab8a5179c428a5557232c3ff74ca9d90343869
AravindVasudev <aravindv96@yahoo.com>
2018-10-29 01:32:00 +0530

    Added simple layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sudoku-solver';
+  board: number[][];
+  dimension = 9;
+  empty = 0;
+
+  constructor() {
+    this.board = this.generateBoard();
+  }
+
+  generateBoard(dimension = this.dimension, fill = this.empty): number[][] {
+    let board = new Array(dimension);
+    for (let i = 0; i < dimension; i++) {
+      board[i] = new Array(dimension).fill(fill);
+    }
+
+    return board;
+  }
 }
