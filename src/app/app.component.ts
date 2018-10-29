a081f94b2499150c7ee62f8abbab31acd00e5dfa
AravindVasudev <aravindv96@yahoo.com>
2018-10-29 22:10:31 +0530

    Added number toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,11 @@ export class AppComponent {
   selected: [number, number] = [-1, -1];
 
   constructor() {
+    // Init board
     this.board = this.generateBoard();
   }
 
+  // create a sqaure board with specified dimension and fill with specified value
   generateBoard(dimension = this.dimension, fill = this.empty): number[][] {
     let board = new Array(dimension);
     for (let i = 0; i < dimension; i++) {
@@ -24,18 +26,25 @@ export class AppComponent {
     return board;
   }
 
+  // select the clicked cell
   select(row: number, col: number) {
     this.selected = [row, col];
   }
 
   @HostListener('window:keydown', ['$event'])
   updateBoard(event: any) {
-    // debugger;
     let num = parseInt(event.key);
-    if (isNaN(num) || this.selected[0] === -1 || num < 0 || num > 9) {
+    if (isNaN(num) || this.selected[0] === -1 || num < 0 || num > 9) { // check if the key is a valid number
       return;
     }
 
+    // toggle number if already present
+    if (num === this.board[this.selected[0]][this.selected[1]]) {
+      this.board[this.selected[0]][this.selected[1]] = this.empty;
+      return;
+    }
+
+    // set number at selected cell
     this.board[this.selected[0]][this.selected[1]] = num;
   }
 }
